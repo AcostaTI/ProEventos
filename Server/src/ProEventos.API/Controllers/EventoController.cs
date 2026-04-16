67ebda083b187708923751d716bf15aec477a633
Alexandre Maciel <ti.alexandre.costa@gmail.com>
2026-04-15 21:12:44 -0300

    Criação contexto

--- Server/src/ProEventos.API/Controllers/EventoController.cs
+++ Server/src/ProEventos.API/Controllers/EventoController.cs
@@ -5,46 +5,45 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ProEventos.API.Models;
 
-namespace ProEventos.API.Controllers
+namespace ProEventos.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class EventoController : ControllerBase
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class EventoController : ControllerBase
+    public IEnumerable<Evento> _eventos = new Evento []
     {
-        public IEnumerable<Evento> _eventos = new Evento []
+        new Evento
         {
+            EventoId = 1,
+            Local = "Pará", 
+            Lote = "1º",
+            Tema = "Comidas Tipícas",
+            DataEvento = DateTime.Now.AddDays(2).ToString("dd/mm/yyyy"),
+            QtdPessoas = 300,
+            ImagemURL = "tacaca.png"
+        },
             new Evento
-            {
-                EventoId = 1,
-                Local = "Pará", 
-                Lote = "1º",
-                Tema = "Comidas Tipícas",
-                DataEvento = DateTime.Now.AddDays(2).ToString("dd/mm/yyyy"),
-                QtdPessoas = 300,
-                ImagemURL = "tacaca.png"
-            },
-             new Evento
-            {
-                EventoId = 2,
-                Local = "Pará",
-                Lote = "3º",
-                Tema = "Danças Regionais",
-                DataEvento = DateTime.Now.AddDays(3).ToString("dd/mm/yyyy"),
-                QtdPessoas = 250,
-                ImagemURL = "carimbo.png"
-            }       
-        };
-
-        [HttpGet]
-        public IEnumerable<Evento> get()
         {
-            return _eventos;   
-        }     
+            EventoId = 2,
+            Local = "Pará",
+            Lote = "3º",
+            Tema = "Danças Regionais",
+            DataEvento = DateTime.Now.AddDays(3).ToString("dd/mm/yyyy"),
+            QtdPessoas = 250,
+            ImagemURL = "carimbo.png"
+        }       
+    };
 
-        [HttpGet("id")]
-        public IEnumerable<Evento> get(int id)
-        {
-            return _eventos.Where(e => e.EventoId == id);   
-        }   
-    }
+    [HttpGet]
+    public IEnumerable<Evento> get()
+    {
+        return _eventos;   
+    }     
+
+    [HttpGet("id")]
+    public IEnumerable<Evento> get(int id)
+    {
+        return _eventos.Where(e => e.EventoId == id);   
+    }   
 }
